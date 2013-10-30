a821675f2cba54acf313552c5bfb3d57d229974e
Traynor <ltraynor@bennington.edu>
2013-10-30 14:07:52 -0400

    API version on with read. Also added client file to execute from

--- APIvers1.py
+++ APIvers1.py
@@ -24,10 +24,10 @@ class API():
 	#lets define some variables
 	global MASTER_ADDRESS
 	global TCP_PORT
-	MASTER_ADDRESS = raw_input("What is the IP of the master? : ")
+	MASTER_ADDRESS = raw_input("What is the IP of the Master? : ")
 	TCP_PORT = config.port
-	
-	#lets make the API a server and a client to send and recieve messages
+
+	#lets make the API able to send and recieve messages
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 	
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.connect((MASTER_ADDRESS, TCP_PORT))
@@ -50,6 +50,7 @@ class API():
 		self.splitdata = self.data.split("|")
 		dataLength = len(self.splitdata)
 		cH = self.splitdata[-1]
+		#spawn threads that connect to the chunk servers
 		for n in range(0, dataLength-1):
 			location = self.splitdata[n]
 			#send this data to chunk servers
@@ -64,9 +65,7 @@ class API():
 	#sends back the chunk handle and locations of the existing file. The 
 	#client then sends "append" and the new data to the chunk servers which
 	#append the new data to the files.
-	def append(self, filename):
-		global newData
-		newData = str(raw_input("Input the data you want to append: "))
+	def append(self, filename, newData):
 		#appends to an existing file
 		self.s.send("APPEND|" + filename)
 		self.data = self.s.recv(1024)
@@ -75,12 +74,35 @@ class API():
 		self.splitdata = self.data.split("|")
 		dataLength = len(self.splitdata)
                 cH = self.splitdata[-1]
+		#spawn threads that connect to the chunk servers
                 for n in range(0, dataLength-1):
                         location = self.splitdata[n]
 	                #send this data to chunk servers
                         thread = chunkConnectAppend(location, cH, newData)
                         thread.start()
 
+	#reads an existing file by first  
+	def read(self, filename, byteOffset, bytesToRead):
+		#send read and the filename to the master
+		self.s.send("READ|" + filename + "|" + str(byteOffset) + "|" + str(bytesToRead))
+		#recieve data from the master
+		self.data = self.s.recv(1024)
+		print self.data
+		#split the data into a list
+		self.splitdata = self.data.split("|")
+		self.splitdata = self.splitdata[1:]
+		print self.splitdata
+		for q in self.splitdata:
+			secondSplit = q.split("*")
+			print secondSplit
+			location = secondSplit[0]
+			print "location = ", location
+			cH = secondSplit[1]
+			print "cH = ", cH
+			offset = secondSplit[2]
+			print "offset = ", offset
+			thread = chunkConnectRead(location, cH, offset, bytesToRead)
+			thread.start()	
                	
 
 #thread for create
@@ -93,8 +115,8 @@ class chunkConnectCreate(threading.Thread):
 		
 	def run(self):
 		self.s.connect((self.location,TCP_PORT))
-		print "makeChunk"
 		self.s.send("makeChunk")
+		print "makeChunk"
 		dat = self.s.recv(1024)
 		if dat == "continue":	
 			print self.cH
@@ -107,20 +129,51 @@ class chunkConnectAppend(threading.Thread):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.location = location
                 self.cH = cH
+		self.newData = newData
               
         def run(self):
                 self.s.connect((self.location,TCP_PORT))
-                self.s.send("append")
-		print "append"
+                self.s.send("APPEND")
+		print "APPEND"
 		dat = self.s.recv(1024)
                	if dat == "continue":
 			self.s.send(self.cH)
 			print self.cH
 		dat = self.s.recv(1024)
 		if dat == "continue":
-			self.s.send(newData)
-			print newData
+			self.s.send(self.newData)
+			print self.newData
 
+#thread for read
+class chunkConnectRead(threading.Thread):
+	def __init__(self, location, cH, offSet, bytesToRead):
+		threading.Thread.__init__(self)
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.location = location
+		self.cH = cH
+		self.offSet = offSet
+		self.bytesToRead = bytesToRead
+	def run(self):
+		self.s.connect((self.location,TCP_PORT))
+		print "tried to connect to chunk server"
+		self.s.send("READ")
+		print "READ"
+		dat = self.s.recv(1024)
+		if dat == "continue":
+			self.s.send(self.cH)
+			print self.cH
+		dat = self.s.recv(1024)
+		if dat == "continue":
+			self.s.send(self.offSet)
+			print self.offSet
+		dat = self.s.recv(1024)
+		if dat == "continue":
+			self.s.send(self.bytesToRead)
+			print self.bytesToRead
+		dat = self.s.recv(2**20)
+		print dat		
+
+#oplog stuff. for questions contact rohail
 class updateOpLog(threading.Thread):
         def __init__(self, message):
                 threading.Thread.__init__(self)
@@ -133,7 +186,6 @@ class updateOpLog(threading.Thread):
 		print "opshit"
 
 
-API = API()
-API.create("/foob/bar")
+
 
 
