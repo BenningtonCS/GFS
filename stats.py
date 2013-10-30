417ff32f7a43dc1cfb4ee4e80ddfcead552cad48
root <root@backus.bennington.edu>
2013-10-29 20:26:54 -0400

    test

--- stats.py
+++ stats.py
@@ -7,16 +7,23 @@ class statGen:
 		self.data = ''
 		try:
 			self.file = open('/var/www/stats.html', 'w')
-			self.file.write('<!DOCTYPE html><html><head><title>Page Title</title><link rel="stylesheet" type="text/css" href="style.css"></script></head><body>')
+			self.file.write('<!DOCTYPE html><html><head><title>GFS Stats</title><link rel="stylesheet" type="text/css" href="style.css"></script></head><body>')
+			self.file.write('<h1>DAT STATZ</h1>')
 		except:
 			print "Couldn't generate stat file!"		
+			exit()
 	def getHosts(self):
 		try: 
+			with open(config.hostsfile, 'r') as self.hostsFile:
+				hosts = self.hostsFile.read().splitlines()
 			with open(config.activehostsfile, 'r') as self.activeHostsFile:
-				hosts = self.activeHostsFile.read().splitlines()
-				string = '<h1>Active Hosts</h1>'
+				activeHosts = self.activeHostsFile.read().splitlines()
+				string = '<h2>Chunkservers</h2>'
 				for x in range(0, len(hosts)):
-					string += hosts[x] + "<br>"
+					if hosts[x] not in activeHosts:
+						string += hosts[x] + " | OFFLINE<br>"
+					elif hosts[x] in activeHosts:
+						string += hosts[x] + " | ONLINE<br>"
 				self.data += string
 		except:
 			self.data += "Couldn't generate stats file!"		
