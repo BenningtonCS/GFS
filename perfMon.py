aec5b715437f5a05f82f3a202461711ce6a775a1
root <root@backus.bennington.edu>
2013-11-09 19:37:26 -0500

    perfmon

--- perfMon.py
+++ perfMon.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+
+import os
+
+disk = os.statvfs("/")
+capacity = disk.f_bsize * disk.f_blocks
+available = disk.f_bsize * disk.f_bavail
+
+try:
+	file = open("httpServerFiles/stats.txt", "w")
+	file.write(str(available/1.073741824e9) +"|" + str(capacity/1.073731824e9))
+except IOError:
+	logging.error("Couldn't make stats file")
+
