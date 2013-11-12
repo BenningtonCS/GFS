a38462cca649115ed286ab86d7de1fb669b38c7d
Kgespada <Cgilbert-Espada@bennington.edu>
2013-11-11 21:14:43 -0500

    finished and verified error handling for interrogateChunkServers

--- database.py
+++ database.py
@@ -185,13 +185,13 @@ class Database:
 			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			data = " "
 			try:
-				s.connect((IP, chunkPort))
+				s.connect(('10.10.117.500', chunkPort))
 				logging.debug('Connection Established: ' + str(IP) + ' on port ' + str(chunkPort))
 				fL.send(s, 'CONTENTS?')
 				logging.debug('Sent chunkserver a CONTENTS? message')
 				data = fL.recv(s)
 			except:
-				print "failed to connect to " + IP
+				logging.error("interrogateChunkServers failed to connect to " + IP)
 
 			
 			s.close()
