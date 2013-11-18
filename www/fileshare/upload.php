46addad43e51d2aa4f2ef461931d9a18baa06aec
root <root@backus.bennington.edu>
2013-11-18 09:25:45 -0500

    www

---
+++ www/fileshare/upload.php
@@ -0,0 +1,25 @@
+<?php
+
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("files/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "files/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "files/" . $_FILES["file"]["name"];
+      }
+    }
+?>
