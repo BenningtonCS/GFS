567ddbced51d268b404ba97f8cbc1e2cd58acb2e
rohailaltaf <rohailaltaf@Rohails-MacBook-Air.local>
2013-11-20 17:06:08 -0500

    logging

--- www/pages/chunkserver.php
+++ www/pages/chunkserver.php
@@ -2,6 +2,8 @@
 
 $server = $_GET['server'];
 $logFile = file_get_contents('http://'.$server.':8000/httpServerFiles/chunkserverLog.log');
+$logFile = explode("\n", $logFile);
+$cpu = $logFile[1];
 ?>
 <script type="text/javascript">
   window.onload = function () {
@@ -16,12 +18,13 @@ $logFile = file_get_contents('http://'.$server.':8000/httpServerFiles/chunkserve
         type: "line",
 
         dataPoints: [
-        { x: 1, y: 450 },
-        { x: 2, y: 414 },
-        { x: 3, y: 520 },
-        { x: 4, y: 460 },
-        { x: 5, y: 450 },
-        { x: 6, y: 500 }
+<?php
+$cpu = explode("|", $cpu);
+for($i=0;$i<60;$i++) {
+  echo '{ x: '.$i.', y: '.$cpu[$i].'},';
+
+}
+?>
         ]
       }
       ]
