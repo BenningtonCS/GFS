6293d73784d498cb82ffa23cbe75e6ec181324d0
rohailaltaf <rohailaltaf@Rohails-MacBook-Air.local>
2013-11-19 21:03:38 -0500

    1

--- www/fileshare/gfs.class.php
+++ www/fileshare/gfs.class.php
@@ -2,25 +2,27 @@
 
 class GFS {
 	public $x;
-	public function __construct() {
+	public function connect() {
 		$this->x = stream_socket_client("tcp://10.10.117.109:6666", $errno, $errorMessage);
 	}
 	public function create($filename) {
+		$this->connect;
 		fwrite($this->x, "CREATE|".$filename);
 		$response = stream_get_contents($this->x);
 		echo $response;
 		return $response;
 	}
 	public function append($filename, $data) {
+		$this->connect;
 		fwrite($this->x, "APPEND|".$filename."|".$data);
 	}
 
 }
 $gfs = new GFS;
 
-$create = $gfs->create('hello/6');
+$create = $gfs->create('hello/7');
 if($create == "CREATE|1") {
-	$gfs->append('hello/6', '123');	
+	$gfs->append('hello/7', '123');	
 }
 
 ?>
