87ab3f850aec131a630dda8b4ae34b61ce1ed9a7
rohailaltaf <rohailaltaf@Rohails-MacBook-Air.local>
2013-11-19 20:12:24 -0500

    test

--- www/fileshare/gfs.class.php
+++ www/fileshare/gfs.class.php
@@ -3,7 +3,7 @@
 class GFS {
 	public $MASTER_IP = '10.10.117.109';
 	public function __construct() {
-		public $x = stream_socket_client("tcp://$MASTER_IP:6666", $errno, $errorMessage);
+		public $x = stream_socket_client("tcp://10.10.117.109:6666", $errno, $errorMessage);
 		fwrite($x, "hello")
 	}
 	public function create($filename) {
