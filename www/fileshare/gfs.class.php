f17a2ec3ec36f7e804b59d066e407c2003adfcda
rohailaltaf <rohailaltaf@Rohails-MacBook-Air.local>
2013-11-19 20:28:15 -0500

    php

--- www/fileshare/gfs.class.php
+++ www/fileshare/gfs.class.php
@@ -1,8 +1,9 @@
 <?php
+
 class GFS {
-	public $x = stream_socket_client("tcp://10.10.117.109:6666", $errno, $errorMessage);
+	public $x;
 	public function __construct() {
-		
+		$this->x = stream_socket_client("tcp://10.10.117.109:6666", $errno, $errorMessage);
 	}
 	public function create($filename) {
 		fwrite($this->x, "CREATE|".$filename);
