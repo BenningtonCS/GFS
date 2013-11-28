dec3a2aa9f594e87b8ffbe7e2d560afbc87e3db3
rohailaltaf <rohailaltaf@Rohails-MacBook-Air.local>
2013-11-27 21:48:46 -0500

    api

--- httpClient.py
+++ httpClient.py
@@ -74,7 +74,8 @@ class processConnection(threading.Thread):
 			if(create == 1):
 				self.connection.send("CREATE|1")
 		elif(msg == "APPEND"):
-			API.append(dataSplit[1], dataSplit[2], True)
+			dataToSend = bytearray(dataSplit[2:])
+			API.append(dataSplit[1], dataToSend)
 		elif(msg == "READ"):
 			read = API.read(dataSplit[1], 0, -1)
 			if(read):
