e0c2d938feb8309ef3bbedc3bd5ade9b11cffb5e
Kgespada <Cgilbert-Espada@bennington.edu>
2013-12-02 19:47:55 -0500

    added index error handling to client.py

--- client.py
+++ client.py
@@ -5,34 +5,41 @@ from API import API
 
 API = API()
 
+try:
+	if (sys.argv[1] == "files"):
+	        API.fileList()
 
-if (sys.argv[1] == "files"):
-        API.fileList()
+	elif sys.argv[1] == "create":
+		filename = sys.argv[2] 
+		API.create(filename)
 
-elif sys.argv[1] == "create":
-	filename = sys.argv[2] 
-	API.create(filename)
+	elif sys.argv[1] == "append":
+		filename = sys.argv[2] 
+		newData = sys.argv[3] 
+		API.append(filename, newData, True)
 
-elif sys.argv[1] == "append":
-	filename = sys.argv[2] 
-	newData = sys.argv[3] 
-	API.append(filename, newData, True)
+	elif sys.argv[1] == "read":
+		filename = sys.argv[2] 
+		byteOffSet = sys.argv[3]
+		bytesToRead = sys.argv[4]
+		newName = sys.argv[5]
+		API.read(filename, byteOffSet, bytesToRead, newName)
 
-elif sys.argv[1] == "read":
-	filename = sys.argv[2] 
-	byteOffSet = sys.argv[3]
-	bytesToRead = sys.argv[4]
-	newName = sys.argv[5]
-	API.read(filename, byteOffSet, bytesToRead, newName)
+	elif sys.argv[1] == "delete":
+		filename = sys.argv[2] 
+		API.delete(filename)
 
-elif sys.argv[1] == "delete":
-	filename = sys.argv[2] 
-	API.delete(filename)
+	elif sys.argv[1] == "undelete":
+		filename = sys.argv[2] 
+		API.undelete(filename)
+		
+	elif (sys.argv[1] == "files"):
+		filename = sys.argv[2] 
+		API.fileList()
 
-elif sys.argv[1] == "undelete":
-	filename = sys.argv[2] 
-	API.undelete(filename)
-	
-elif (sys.argv[1] == "files"):
-	filename = sys.argv[2] 
-	API.fileList()
+except IndexError:
+	if len(sys.argv) < 2:
+		print "No command line arguments found, please run the code with a command an input."
+	else:
+		print "command line error, input not correct."
+	exit(1)
