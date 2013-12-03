5ce0674a8739af943042d9c6ea4bbcbe745b5761
VulpesArtificem <tglenn@bennington.edu>
2013-12-03 00:16:13 -0500

    Update readwhizzifest.py

--- readwhizzifest.py
+++ readwhizzifest.py
@@ -13,10 +13,10 @@ with open("machineFunction.txt","r") as machineList:
  			machineType =  SplitLine[1]
 
 if machineType == "C":
- 	with open('whizzifestC.txt') as w:
+ 	with open('whizzifestC.txt','r') as w:
         content = w.readlines()
 elif machineType == "M":
-    with open('whizzifestM.txt') as w:
+    with open('whizzifestM.txt','r') as w:
         content = w.readlines()
 # open the whizzifest.txt file that has been manually generated. This 
 # puts the file into a list named content, each line being its own item.
