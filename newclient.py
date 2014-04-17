7408e4f27c1b415746226396b4460c43f64ec346
Erick Daniszewski <edaniszewski@gmail.com>
2014-04-17 03:59:50 -0400

    added more features to GUI
    can now upload and download files,
    server connect and disconnect buttons will be phased out,
    added buttons for delete and undelete (need to be implemented still),
    added refresh list functionality, and rework-ed the GUI to look better.

--- newclient.py
+++ newclient.py
@@ -2,8 +2,9 @@
 
 from Tkinter import Tk, Text, BOTH, W, N, E, S, Listbox, END, RIGHT, Y, Scrollbar
 from ttk import Frame, Button, Label, Style
-import socket, re
+import socket, re, tkFileDialog
 import functionLibrary as fL
+from API import API
 
 
 class GUI(Frame):
@@ -11,10 +12,14 @@ class GUI(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)   
 
+        self.api = API()
+
         self.SERVER_IP = '10.10.100.144'
         self.SERVER_PORT = 9666
         self.serverStatus = 0
-         
+        self.filesList = []
+
+
         self.parent = parent
         
         self.initUI()
@@ -27,49 +32,91 @@ class GUI(Frame):
         self.style.theme_use("default")
         self.pack(fill=BOTH, expand=1)
 
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
-        
-        self.lbl = Label(self, text="Bennington File System Server Status:")
-        self.lbl.grid(sticky=W, pady=4, padx=5)
 
-
-        self.connectToServer()
-
-
-        self.area = Listbox(self)
-        self.area.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E+W+S+N)
+        self.file_opt = options = {}
+        options['defaultextension'] = ''
+        options['filetypes'] = ''
+        options['initialdir'] = 'C:\\'
+        options['initialfile'] = 'myfile.txt'
+        options['parent'] = self
+        options['title'] = 'This is a title'
 
         
-        if self.serverStatus == 1:
-            self.getFiles()
+        lbl = Label(self, text="Bennington File System Server Status:", foreground="black")
+        lbl.grid(column=0, row=0, pady=4, padx=5)
+
+        self.status = Label(self, text="", foreground="black")
+        self.status.grid(column=3, row=0, pady=4, padx=5)
+
+
+        #self.connectToServer()
+
+
+        self.area = Listbox(self, height=20)
+        self.area.grid(row=1, column=0, columnspan=1, rowspan=7, padx=5, sticky=N+W+E+S)
+
+        
+        self.getFiles()
         
 
-        reconn = Button(self, text="Connect", command=self.connectToServer)
+        reconn = Button(self, text="Connect")
         reconn.grid(row=1, column=3)
 
-        cbtn = Button(self, text="Disconnect", command=self.disconnectFromServer)
+        cbtn = Button(self, text="Disconnect")
         cbtn.grid(row=2, column=3, pady=4)
 
-        #abtn = Button(self, text="Activate")
-        #abtn.grid(row=3, column=3)
+        abtn = Button(self, text="Upload", command=self.openFile)
+        abtn.grid(row=3, column=3)
+
+        dwnbtn = Button(self, text="Download", command=self.downloadFile)
+        dwnbtn.grid(row=4, column=3)
         
+        delbtn = Button(self, text="Delete", command=self.deleteFile)
+        delbtn.grid(row=5, column=3)
+
+        undelbtn = Button(self, text="Undelete")
+        undelbtn.grid(row=6, column=3)
+
+        refbtn = Button(self, text="Refresh List", command=self.getFiles)
+        refbtn.grid(row=7, column=3)
+
         #hbtn = Button(self, text="Help")
         #hbtn.grid(row=5, column=0, padx=5)
 
         #obtn = Button(self, text="OK")
         #obtn.grid(row=5, column=3)
 
-        quitButton = Button(self, text='Quit', command=self.exitProgram)
-        quitButton.grid(row=5, column=3) 
 
+
+        quitButton = Button(self, text='Quit', command=self.exitProgram)
+        quitButton.grid(sticky=W, padx=5, pady=4) 
+
+
+    def downloadFile(self):
+        fileName = self.currentSelection()
+        self.api.read(fileName, 0, -1, fileName)
+
+
+    def currentSelection(self):
+        index = self.area.curselection()[0]
+        fileName = self.area.get(index)
+        return fileName
+
+
+    def deleteFile(self):
+        print "(:"
+
+
+    def openFile(self):
+        filepath = tkFileDialog.askopenfilename(**self.file_opt)
+        filename = filepath.split('/')[-1]
+
+        self.api.create(filename)
+        self.api.append(filename, filepath, 1)
 
     def getFiles(self):
         try:
-            fL.send(self.s, "FILELIST|x")
-            data = fL.recv(self.s)
+            data = self.api.fileList()
             
             data = re.split('\*', data)
             
@@ -77,12 +124,14 @@ class GUI(Frame):
             for item in data:
                 tempitem = re.split('\^', item)
                 for thing in tempitem:
-                    if "|" in thing:
+                    if "|" in thing and not thing == "|":
                         fileNames.append(thing.strip('|'))
 
 
             for item in fileNames:
-                self.area.insert( END, item )
+                if item not in self.filesList:
+                    self.area.insert( END, item )
+                    self.filesList.append(item)
 
 
         except Exception as e:
@@ -111,19 +160,22 @@ class GUI(Frame):
             print "couldnt connect"
 
         if self.serverStatus:
-            self.lbl['text'] = "Bennington File System Sever Status: Connected"
+            self.status['text'] = "Connected"
+            self.status['foreground'] = "black"
         elif not self.serverStatus:
-            self.lbl['text'] = "Bennington File System Sever Status: Failed to Connect"
+            self.status['text'] = "Failed to Connect"
+            self.status['foreground'] = "orange"
         else:
-            self.lbl['text'] = "Error occured when attempting to connect to server"
+            self.status['text'] = "Error"
 
 
 
     def disconnectFromServer(self):
         try:
-            self.s.close()
+            #self.s.close()
             print "disconnecting"
-            self.lbl['text'] = "Bennington File System Sever Status: Disconnected"
+            self.status['text'] = "Disconnected"
+            self.status['foreground'] = "red"
 
         except Exception as e:
             raise e
@@ -138,7 +190,7 @@ class GUI(Frame):
 def main():
   
     root = Tk()
-    root.geometry("550x400+400+400")
+    root.geometry("420x430+400+400")
     app = GUI(root)
     root.mainloop()  
 
